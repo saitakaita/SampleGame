576c85fdf76171902d7bc4af5c03bdad3ffb6963
Yamada Tomohiko <saitakaita2@gmail.com>
2013-02-17 22:21:53 +0900

    Initial Commit

---
+++ BlockGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BlockGame
+//
+//  Created by tomohiko on 2013/02/17.
+//  Copyright (c) 2013年 jp.main.yamato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BlockGame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BlockGame
+//
+//  Created by tomohiko on 2013/02/17.
+//  Copyright (c) 2013年 jp.main.yamato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
